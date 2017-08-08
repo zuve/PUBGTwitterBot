cf40d00ce0bd94ec7f0254113e303361500e6e42
Zuve <zuvedev@gmail.com>
2017-08-08 02:39:29 -0600

    Removes IDs from log.txt, changes keyword to just fix and removes replies from being posted

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import tweepy, praw, os, json, datetime
+import tweepy, praw, os, json
 from HTMLParser import HTMLParser
 
 #HTML Parser for Tweet Text/Reddit Title
 h = HTMLParser()
 
 #Logging variables
-log = open("log.txt", "r+")
+log = open("log.txt", "r")
 
 posted = [post.rstrip('\n') for post in log]
+
+log.close()
 
 #Twitter variables
 TWITTER_CONSUMER_KEY = os.environ['PUBGBOT_CONSUMER_KEY']
@@ -28,12 +30,15 @@ REDDIT_PASSWORD = os.environ['REDDIT_PASSWORD']
 reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, password=REDDIT_PASSWORD, user_agent='PUBG Twitter Bot 0.1', username=REDDIT_USERNAME)
 subreddit = reddit.subreddit("PUBATTLEGROUNDS")
 
-log = open("log.txt", "r+")
+log = open("log.txt", "a")
 for t in recent_tweets:
     recent_tweet = h.unescape(t.text)
     recent_tweet_url = "https://twitter.com/statuses/" + str(t.id)
     recent_tweet_id = str(t.id)
-    if "pushed a hotfix" in recent_tweet or "pushed an update" in recent_tweet or "resolved" in recent_tweet or "fixed the bug" in recent_tweet:
-        if not any(recent_tweet_id in s for s in posted):
-            subreddit.submit(recent_tweet, url=recent_tweet_url).flair.select('ba18c8f4-14b8-11e7-a6c6-0e11d8c4f614', text=None)
-            log.write(recent_tweet_id + "\n")
+    reply_status = t.in_reply_to_status_id
+    if reply_status is None:
+        if "fix" in recent_tweet:
+            if not any(recent_tweet_id in s for s in posted):
+                subreddit.submit(recent_tweet, url=recent_tweet_url).flair.select('ba18c8f4-14b8-11e7-a6c6-0e11d8c4f614', text=None)
+                log.write(recent_tweet_id + "\n")
+log.close()
